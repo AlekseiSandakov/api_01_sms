1c7d1d4946fe23a76a766b1b23b8b76fa2743806
Aleksei <axe455990@mail.ru>
2021-02-04 09:39:33 +0700

    My first commit

--- homework.py
+++ homework.py
@@ -27,7 +27,7 @@ def get_status(user_id):
                          params=params).json()['response'][0]['online']
 
 
-def send_sms(sms_text):
+def sms_sender(sms_text):
     message = client.messages.create(
         body=sms_text,
         from_=number_from,
@@ -40,6 +40,6 @@ if __name__ == '__main__':
     vk_id = input('Введите id')
     while True:
         if get_status(vk_id) == 1:
-            send_sms(f'{vk_id} сейчас онлайн!')
+            sms_sender(f'{vk_id} сейчас онлайн!')
             break
         time.sleep(5)
